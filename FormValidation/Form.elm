2f78222bd24b9896a3c09b854e9bcb334eda3204
Kvalheimo <ma_kval@hotmail.com>
2018-06-14 22:30:18 +0200

    push

--- FormValidation/Form.elm
+++ FormValidation/Form.elm
@@ -9,8 +9,9 @@ import Json.Decode as Json
 import Validation exposing (..)
 
 type alias Model =
-  { email : String
-  , message : String
+  { email : Field String
+  , message : Field String
+  , age : OptionalField Int
   , status : SubmissionStatus
   }
 
@@ -22,8 +23,9 @@ type SubmissionStatus
 
 initialModel : Model
 initialModel =
-  { email = ""
-  , message = ""
+  { email = NotValidated ""
+  , message = NotValidated ""
+  , age = NotValidated ""
   , status = NotSubmitted
   }
 
@@ -31,6 +33,7 @@ type Msg
   = InputEmail String
   | InputMessage String
   | Submit
+  | InputAge String
   | SubmitResponse (Result Http.Error () )
 
 main : Program Never Model Msg
@@ -46,42 +49,67 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of
         InputEmail e ->
-            ({model | email = String.toLower e}, Cmd.none)
+            ({model | email = NotValidated e}, Cmd.none)
         InputMessage m ->
-            ({model | message = m}, Cmd.none)
+            ({model | message = NotValidated m}, Cmd.none)
+        InputAge a ->
+            ({model | age = NotValidated a}, Cmd.none)
         Submit ->
-            submitIfValid model
+            model |> validateModel |> submitIfValid
         SubmitResponse (Ok ()) ->
             ({model | status = Succeded
-            , email =""
-            , message = ""}, Cmd.none)
+            , email = NotValidated ""
+            , message = NotValidated ""}, Cmd.none)
         SubmitResponse (Err _) ->
             ({model | status = Failed}, Cmd.none)
+
+validateModel : Model -> Model 
+validateModel model = 
+  let
+    emailValidation = 
+      isNotEmpty "An email is required"
+      >=> isEmail "Please ensure this is a valid email"
+
+    email = model.email |> validate emailValidation
+
+    message = model.message
+      |> validate (isNotEmpty "A mesage is required")
+
+    age = model.age
+      |> validate (optional (isNatural "I'm expecting a positive number"))
+
+
+  in
+    {model | email = email
+    , message = message
+    , age = age
+    }
 
 submitIfValid : Model -> (Model, Cmd Msg)
 submitIfValid model =
   let
-    submissionResult : Result String (Cmd Msg)
     submissionResult = 
-    Result.map2
-      submit
-      (model.email |> isEmail) --Result String String
-      (model.message |> isNotEmpty) -- Result Strnig String
+      Valid submit
+        |: model.email
+        |: model.message 
+        |: model.age
     
   in case submissionResult of
-    Ok cmd ->
+    Valid cmd ->
       ({model | status = InProcess}, cmd)
-    Err _ ->
+    _ ->
       (model, Cmd.none)
             
-submit : String -> String -> Cmd Msg
-submit email message =
+submit : String -> String -> Maybe Int ->  Cmd Msg
+submit email message age =
     let
         url = "http://localhost:3000/api/contact"
 
         json = Encode.object
             [ ("email", Encode.string email)
             , ("message", Encode.string message)
+            , ("age", age |> Maybe.map Encode.int
+                          |> Maybe.withDefault Encode.null )
             ]
 
         decoder = Json.string |> Json.map (always () )
@@ -117,25 +145,48 @@ renderStatus status =
             div [] [text "Your request has been received"]
         Failed ->
             div [class "alert alert-danger"] 
-            [text "ops! There was an error, please try again"]
+            [ text "Ops! There was an error, please try again"]
+
+errorLabel : Field a -> Html Msg
+errorLabel field = label 
+      [ class "label lable-error" ]
+      [ field 
+        |> extractError 
+        |> Maybe.withDefault "" 
+        |> text
+      ]
 
 body model = div []
   [ div []
     [ input
-      [ placeholder "your email"
+      [ placeholder "your email *"
       , type_ "email"
       , onInput InputEmail
-      , value model.email
+      , value (model.email |> displayValue identity)
       , required True
-      ] [] ]
+      ] []
+      , errorLabel model.email
+    ]
   , div []
     [ textarea
-      [ placeholder "your message"
+      [ placeholder "your message *"
       , rows 7
       , onInput InputMessage
-      , value model.message
+      , value (model.message |> displayValue identity)
       , required True
-      ] [] ]
+      ] []
+      , errorLabel model.message 
+      ]
+    
+  , div []
+    [ input
+      [ placeholder "your age"
+      , onInput InputAge
+      , value (model.age 
+      |> displayValue (Maybe.map toString >> Maybe.withDefault ""))
+      ] []
+      , errorLabel model.age 
+      ]
   ]
 
 footer model = div []
